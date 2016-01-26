7deaad15cf37d6dea2cadce3ce7494c7920fe55e
boxizen <boxizen@gmail.com>
2016-01-26 21:52:37 +0800

    [ADD] 添加自适应窗体变化逻辑

--- src/boot.js
+++ src/boot.js
@@ -1,8 +1,23 @@
+// 初始化整体样式
 require("!style!css!sass!./css/main.scss");
 
+// 引入组件
 var toolbar = require('./components/toolbar/toolbar.js');
 var setting = require('./components/setting/setting.js');
+toolbar.init('.widget-wrapper');
+setting.init('.widget-wrapper');
 
-toolbar.init('.wrapper');
 
-setting.init('.wrapper');
+// 自适应窗口变化
+resize();
+$(window).bind('resize', resize);
+
+/**
+ * 根据窗口大小动态调整幻灯片大小
+ * @api private
+ */
+function resize() {
+    var winW = $(window).width(),
+        winH = $(window).height();
+    $('.edit-container').css('width', winW - 120 + 'px');
+}
