b6ef267a6230bc3307c93c3db630b4b4cb76339b
boxizen <boxizen@gmail.com>
2016-01-28 17:14:41 +0800

    [UPD] 完成div移动

--- src/components/edit-panel/edit-panel.js
+++ src/components/edit-panel/edit-panel.js
@@ -19,10 +19,10 @@ module.exports = {
     bind: function() {
         // 销毁对象
         $('.edit-container').click(function() {
+            // 还原编辑器覆盖的区块                  
+            $('.anchor-block').removeClass('focus');
+            $('.edit-div').attr('contenteditable', false).attr('contenteditable', false).removeClass('focus').removeClass('cke_editable').removeClass('editing');
             for (var editor in CKEDITOR.instances) {
-                // 还原编辑器覆盖的区块                  
-                $('#' + editor).parent().children('.anchor-block').removeClass('focus');
-                $('#' + editor).attr('contenteditable', false).removeClass('focus').removeClass('cke_editable');
                 // 销毁编辑器实例  
                 CKEDITOR.remove(CKEDITOR.instances[editor]);
                 // 移除编辑器Jquery对象  
@@ -32,13 +32,42 @@ module.exports = {
 
         // 单击事件
         $('.edit-div').click(function(e) {
-            if ($(this).hasClass('focus')) {
-                console.log('has focus');
+            if ($(this).hasClass('focus') && !$(this).hasClass('editing')) {
+                $(this).addClass('editing');
                 CKEDITOR.inline($(this).attr('id'));
             }
-            $(this).attr('contenteditable', true).addClass('focus');
-            $(this).parent().children('.anchor-block').addClass('focus');
-            e.stopPropagation();            
+            $(this).attr('contenteditable', true).addClass('focus').parent().children('.anchor-block').addClass('focus');
+            e.stopPropagation();
+        })
+
+        // 拖动div
+        $('.edit-block').mousedown(function(e) {
+            var isMove = true;
+            var firstX = e.pageX;
+            var firstY = e.pageY;    
+            var staticLeft = $(this).css('left');
+            var staticTop = $(this).css('top');
+            $(this).addClass('focus').parent().children('.anchor-block').addClass('focus');            
+            var that = this;
+            $(document).mousemove(function(event) {
+                if(isMove) {
+                    // 计算最终x坐标
+                    var curX = event.pageX;
+                    var distanceX = curX - firstX;
+                    var finalX = parseInt(staticLeft) + distanceX;
+                    $(that).css('left', finalX + 'px');
+                    //计算最终y坐标
+                    var curY = event.pageY;
+                    var distanceY = curY - firstY;
+                    var finalY = parseInt(staticTop) + distanceY;                    
+                    $(that).css('top', finalY + 'px');                    
+                }
+            }).mouseup(function(e){
+                isMove = false;
+                $(document).unbind('mousemove');
+            });
+
+            e.stopPropagation();
         })
     }
 }
